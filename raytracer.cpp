8abf821dce048ae45d815f1cbee28e91993a278f
johnaloomis <johnaloomis@ufl.edu>
2018-04-11 20:23:42 -0400

    Added plane
    Added Plane that disappears into the distance.

--- raytracer.cpp
+++ raytracer.cpp
@@ -73,6 +73,40 @@ struct Sphere{
   }
 
 };
+
+struct plane
+{
+    Vector point;
+    Vector normal;
+    Vector color;
+    double kk,ks,ka;
+    int pow;
+    plane(Vector po, Vector n,Vector c, double k1, double k2, double k3, int p)
+    {
+        point = po;
+        normal = n;
+        color = c;
+        kk = k1;
+        ks = k2;
+        ka = k3;
+        pow = p;
+
+    }
+
+    bool intersect(Vector RayOri,  Vector RayDir, double &t)
+    {
+        double ln = dot(normal,RayDir);
+        if(ln > 1e-6)
+        {
+
+            Vector PROri = point - RayOri;
+            t = dot(PROri,normal);
+            return(t >= 0);
+        }
+        return false;
+    }
+};
+
 struct Object
 {
     Sphere sphere = Sphere(Vector(0,0,0),0);
@@ -92,13 +126,19 @@ struct Object
 };
 
 
+
+
 int main(){
 
-  double intensity = .5;
+    double intensity = .5;
   Sphere sphere1(Vector(150,150,50),50);
   Sphere sphere2(Vector(35,175,50),25);
   Vector blue(0,87,255);
   Vector green(60,167,28);
+    Vector red(255,80,80);
+
+    plane p(Vector(128,128,100),Vector(0,-2,1),red,.5,1.5,1,50);
+
   Object objs[2] = {Object(sphere1,blue,.5,1.5,1,50),Object(sphere2,green,1,1.5,1,50)} ;//you can add spheres as objects and I could expand it to other objects
   //Compute u,v,w basis vectors
   //Creating blank 256x256 image
@@ -107,7 +147,6 @@ int main(){
   //for each pixel
   for (int y = 0; y<256; y++){
     for(int x=0;x<256;x++){
-      for(int i = 0;i<sizeof(objs)/sizeof(objs[0]);i++) {
         //---compute viewing ray---//
         //distance from eye to point on half-line set purposely to a very high number
         double t = std::numeric_limits<double>::infinity();
@@ -119,36 +158,78 @@ int main(){
         //Vector d = s-e;
         //ray e+t*d
         Vector viewRay;
-        if (objs[i].sphere.intersect(e, d, t) && t > 0) {
-          //set pixel color
-          //light source top middle
-          viewRay = e + d * t;
-          Vector light = Vector(0, 1, 1);
-          Vector normal = objs[i].sphere.normal(viewRay);
-          Vector r = normal * (2 * dot(normal, light)) - light;
-          // double lightDis = sqrt((light.x-sphere1.origin.x)*(light.x-sphere1.origin.x) + (light.y-sphere1.origin.y)*(light.y-sphere1.origin.y) + (light.z-sphere1.origin.z)*(light.z-sphere1.origin.z));
-          double lk = objs[i].kk *(intensity) * std::max(0.0, dot(normal.normalize(), light.normalize()));
-          double ls = objs[i].ks * (intensity) * pow(std::max(0.0, dot(normal.normalize(), r.normalize())), objs[i].pow);
-          double la = objs[i].ka * (intensity);
-          if (objs[i].color.x * la + 255 * ls + objs[i].color.x * lk > 255) {
-            img(x, y, 0) = 255;
-          } else {
-            img(x, y, 0) = objs[i].color.x * la + 255 * ls + objs[i].color.x * lk;
-          }
-          if (objs[i].color.y * la + 255 * ls + objs[i].color.y * lk > 255) {
-            img(x, y, 1) = 255;
-          } else {
-            img(x, y, 1) = objs[i].color.y * la + 255 * ls + objs[i].color.y * lk;
-          }
-          if (objs[i].color.z * la + 255 * ls + objs[i].color.z * lk > 255) {
-            img(x, y, 2) = 255;
-          } else {
-            img(x, y, 2) = objs[i].color.z * la + 255 * ls + objs[i].color.z * lk;
-          }
+
+        if (p.intersect(e, d, t) && t > 0) {
+
+            //set pixel color
+            //light source top middle
+            viewRay = e + d * t;
+            Vector light = Vector(0, 1, 1);
+            Vector normal = p.normal;
+            Vector r = normal * (2 * dot(normal, light)) - light;
+            double la = p.ka * (intensity);
+            la = (la * y/192);
+            if (p.color.x * la > 255) {
+                img(x, y, 0) = 255;
+            } else {
+                img(x, y, 0) = p.color.x * la;
+            }
+            if (p.color.y * la > 255) {
+                img(x, y, 1) = 255;
+            } else {
+                img(x, y, 1) = p.color.y * la;
+            }
+            if (p.color.z * la > 255) {
+                img(x, y, 2) = 255;
+            } else {
+                img(x, y, 2) = p.color.z * la;
+            }
         }
+
+      for(int i = 0;i<sizeof(objs)/sizeof(objs[0]);i++) {
+          //---compute viewing ray---//
+          //distance from eye to point on half-line set purposely to a very high number
+          double t = std::numeric_limits<double>::infinity();
+          //position of eye
+          Vector d = Vector(0, 0, 1);
+          //position of pixel
+          Vector e = Vector(x, y, 0);
+          //find first object hit by ray and its surface normal n
+          //Vector d = s-e;
+          //ray e+t*d
+          Vector viewRay;
+          if (objs[i].sphere.intersect(e, d, t) && t > 0) {
+              //set pixel color
+              //light source top middle
+              viewRay = e + d * t;
+              Vector light = Vector(0, 1, 1);
+              Vector normal = objs[i].sphere.normal(viewRay);
+              Vector r = normal * (2 * dot(normal, light)) - light;
+              // double lightDis = sqrt((light.x-sphere1.origin.x)*(light.x-sphere1.origin.x) + (light.y-sphere1.origin.y)*(light.y-sphere1.origin.y) + (light.z-sphere1.origin.z)*(light.z-sphere1.origin.z));
+              double lk = objs[i].kk * (intensity) * std::max(0.0, dot(normal.normalize(), light.normalize()));
+              double ls = objs[i].ks * (intensity) *
+                          pow(std::max(0.0, dot(normal.normalize(), r.normalize())), objs[i].pow);
+              double la = objs[i].ka * (intensity);
+              if (objs[i].color.x * la + 255 * ls + objs[i].color.x * lk > 255) {
+                  img(x, y, 0) = 255;
+              } else {
+                  img(x, y, 0) = objs[i].color.x * la + 255 * ls + objs[i].color.x * lk;
+              }
+              if (objs[i].color.y * la + 255 * ls + objs[i].color.y * lk > 255) {
+                  img(x, y, 1) = 255;
+              } else {
+                  img(x, y, 1) = objs[i].color.y * la + 255 * ls + objs[i].color.y * lk;
+              }
+              if (objs[i].color.z * la + 255 * ls + objs[i].color.z * lk > 255) {
+                  img(x, y, 2) = 255;
+              } else {
+                  img(x, y, 2) = objs[i].color.z * la + 255 * ls + objs[i].color.z * lk;
+              }
+          }
       }
+
     }
   }
-  img.display();
-  return 0;
+    img.display();
+    return 0;
 }
